f25fda9a8aad170cccb2ae73c55ef4538184c2b9
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2022-04-15 14:12:22 -0300

    feat(routes): add dummy Breeds, Favorites and Profile routes
    Add some dummy pages/routes for 'Breeds', 'Favorites' and 'Profile'.

---
+++ src/routes/Breeds/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Breeds = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Breeds</Text>
+    </View>
+  );
+};
+
+export default Breeds;
---
+++ src/routes/Favorites/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Favorites = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Favorites</Text>
+    </View>
+  );
+};
+
+export default Favorites;
