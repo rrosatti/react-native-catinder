b185af4be0f6e9948260b586f0f84d42db13902c
Rodrigo Rosatti Galvão <rodrigorosattig@gmail.com>
2022-04-15 16:03:14 -0300

    refactor(Breeds): use the BreedStack component to display mocked breeds
    This patch is adding some "breed mocked info" and displaying them using the
    'BreedStack' component. It will be temporary while there's no communication
    with the server.

--- src/routes/Breeds/index.tsx
+++ src/routes/Breeds/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+
+import type { BreedTileProps } from '../../components/BreedTile';
+import BreedStack from '../../components/BreedStack';
 
 import styles from './styles';
+
+const mockedInfo: BreedTileProps = {
+  id: 'testing',
+  imgSrc: require('../../assets/mocked-cat.png'),
+  name: 'Abyssinian',
+  lifeSpan: '4',
+  origin: 'Egypt',
+};
+
+const mockedInfo2: BreedTileProps = {
+  id: 'testing2',
+  imgSrc: require('../../assets/mocked-cat.png'),
+  name: 'Another Cat',
+  lifeSpan: '7',
+  origin: 'France',
+};
+
+const mockedInfo3: BreedTileProps = {
+  id: 'testing3',
+  imgSrc: require('../../assets/mocked-cat.png'),
+  name: 'Again, other cat',
+  lifeSpan: '19',
+  origin: 'Canada',
+};
 
 const Breeds = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <Text>Breeds</Text>
+      <BreedStack items={[mockedInfo, mockedInfo2, mockedInfo3]} />
     </View>
   );
 };
